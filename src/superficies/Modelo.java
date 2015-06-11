0631a22c8bafb7e4932308e3020cc65b3d5b5bad
rzarzafernandez <rzarzafernandez@atanasoft32.danielcastelao.org>
2015-06-11 09:00:14 +0200

    creadas las variables y sus getter and setters necesarios

--- src/superficies/Modelo.java
+++ src/superficies/Modelo.java
@@ -12,4 +12,43 @@ package superficies;
  */
 public class Modelo {
     
+    String shapeType; 
+    float side1;
+    float side2; 
+    float area; 
+
+    public String getShapeType() {
+        return shapeType;
+    }
+
+    public void setShapeType(String shapeType) {
+        this.shapeType = shapeType;
+    }
+
+    public float getSide1() {
+        return side1;
+    }
+
+    public void setSide1(float side1) {
+        this.side1 = side1;
+    }
+
+    public float getSide2() {
+        return side2;
+    }
+
+    public void setSide2(float side2) {
+        this.side2 = side2;
+    }
+
+    public float getArea() {
+        return area;
+    }
+
+    public void setArea(float area) {
+        this.area = area;
+    }
+    
+    
+
 }
