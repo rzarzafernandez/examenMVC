195b5b6b1ef403fc508b9840116fe264eea5608d
rzarzafernandez <rzarzafernandez@atanasoft32.danielcastelao.org>
2015-06-11 09:41:30 +0200

    añadida presentacion

--- src/superficies/Presentacion.java
+++ src/superficies/Presentacion.java
@@ -6,10 +6,51 @@
 
 package superficies;
 
+import java.util.Scanner;
+
 /**
  *
  * @author rzarzafernandez
  */
 public class Presentacion {
+    private Scanner sc = new Scanner(System.in);
+    
+    public Modelo chooseArea(Modelo object) {
+        System.out.println("What shape would you like to find the area of? (square, circle, triangle, rectangle):");
+        object.shapeType = sc.nextLine();
+        System.out.println("You said: " + object.shapeType);
+        return object;
+    }
+    
+    public Modelo dataSquare(Modelo object) {
+        System.out.println("what is the side length?");
+        object.side1 = sc.nextFloat();
+        return object;
+    }
+    
+    public Modelo dataRectangle(Modelo object) {
+        System.out.println("what is the rectangles width?");
+        object.side1 = sc.nextFloat();
+        System.out.println("What is the rectangles height?");
+        object.side2 = sc.nextFloat();
+        return object;
+    }
+    
+    public Modelo dataTriangle(Modelo object) {
+        System.out.println("What is the base length of the triangle?");
+        object.side1 = sc.nextFloat();
+        System.out.println("What is the height of the triangle?");
+        object.side2 = sc.nextFloat();
+        return object;
+    }
+    
+    public Modelo dataCircle(Modelo object) {
+        System.out.println("What is the radius of the circle?");
+        object.side1 = sc.nextFloat();
+        return object;
+    }
+    public void printArea(Modelo object) {
+        System.out.println("The area for your " + object.shapeType + " : " + object.area);
+    }
     
 }
